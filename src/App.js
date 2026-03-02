0cb3efe8cf442b0b0dd8b54d85d3342633d8eae0
Siva290395 <sivaphudur@gmail.com>
2026-03-02 17:08:05 +0530

    frontend connected to backend

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
- return (
-  <h1>ABC Company Frontend Application - Version 2</h1>
- )
+
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://35.168.90.51:5000")
+      .then(res => res.text())
+      .then(data => setMessage(data))
+      .catch(err => console.log(err));
+  }, []);
+
+  return (
+    <div>
+      <h1>ABC Company Frontend Application - Version 3</h1>
+      <h2>{message}</h2>
+    </div>
+  );
 }
+
 export default App;
